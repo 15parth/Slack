43bfce2296dd9e5f8e709b80c7d3a9e85fab4684
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-05-24 13:57:25 +0530

    changes for user model addition

---
+++ src/respositories/userRepositories.js
@@ -0,0 +1,18 @@
+import User from "../schema/user"
+import crudRepository from "./crudRepositories";
+
+
+ const userRepository ={
+    ...crudRepository(User),
+    getUserByEmail : async(email) => {
+    const user = await User.findOne((email))
+    return user;
+},
+ getUserByName : async(name) => {
+    const user= await User.findOne({name})
+    return user
+}
+
+}
+
+export default userRepository
