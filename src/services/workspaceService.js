6c935e54bf9c4834ee5c9a99ea0fd39ac0e76f8f
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-05-27 06:59:08 +0530

    changes

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -3,9 +3,10 @@ import {v4 as uuidv4} from 'uuid'
 import workspaceRepository from '../respositories/workspaceRepository.js'
 
 export const createWorkspaceService = async (workspaceData)=>{
-    const joinCode = uuidv4().substring(0,6);
+   try{
+     const joinCode = uuidv4().substring(0,6);
    
-    console.log('this is workspaceData', workspaceData)
+    // console.log('this is workspaceData', workspaceData)
 
 
     const response = await workspaceRepository.create({
@@ -15,19 +16,23 @@ export const createWorkspaceService = async (workspaceData)=>{
 
     });
 
-    //  console.log('workspace data ---->',workspaceData, '& this is the response -->', response)
 
 
-   const addMemberReponse =  await workspaceRepository.addmemberToWorkspace(
+      await workspaceRepository.addmemberToWorkspace(
         response._id,
         workspaceData.owner,
         'admin'
      );
 
-    //  console.log('workspace data ---->',workspaceData,' &  this is response ---> ',addMemberReponse)
 
-     await workspaceRepository.addChannelsToWorkspace(response._id,'general')
+    const updatedWorkspace=  await workspaceRepository.addChannelsToWorkspace(response._id,'general')
 
-     return response;
+     return updatedWorkspace;
+   }
+   catch(error){
+      console.log(error)
+      throw error;
+   }
+   
 };
 
