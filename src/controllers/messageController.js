309abbe29225eb40fc2e7cdf86a31ff294e789bb
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-06-08 17:36:40 +0530

    message fetching added

---
+++ src/controllers/messageController.js
@@ -0,0 +1,27 @@
+import { StatusCodes } from "http-status-codes"
+import { customErrorResponse, internalServerError, successResponse } from "../utils/common/responseObjects.js"
+import { getMessagesService } from "../services/messageService.js"
+
+export const getMessages= async(req,res)=>{
+     try{
+        
+           const message = await getMessagesService({
+            channelId: req.query.channelId,
+            workspaceId: req.query.workspaceId,
+
+           },
+          req.query.page || 1,
+          req.query.limit || 20,
+          req.user
+        )
+           return res.status(StatusCodes.OK).json(successResponse(message , 'Messages fetched successfully'))
+        }
+        catch(error){
+            console.log("Message controller error", error)
+            if(error.statusCode){
+                return res.status(error.statusCode).json(customErrorResponse(error))
+            }
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json(internalServerError(error))
+        }
+}
---
+++ src/routes/v1/messages.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import messageController from '../../controllers/messageController.js'
+
+import { isAuthenticated } from '../../middleware/authMiddleware.js';
+
+const router =  express.Router();
+
+router.get('/messages/:channelId',
+     isAuthenticated, 
+     messageController
+    )
+
+export default router;
